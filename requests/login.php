bd62cda6f4bf618b7cc1f7a63eeaea78e86401cb
Sepehr <sepehr.gouran@gmail.com>
2018-06-19 15:10:24 +0430

    accept JSON body

--- requests/login.php
+++ requests/login.php
@@ -5,16 +5,21 @@
  * Date: 6/14/2018
  * Time: 10:01 AM
  */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 require_once("../results/LoginResult.php");
 
 session_start();
 include '../database/database.php';
 
-if (isset($_POST['login'])) {
+$data = json_decode(file_get_contents('php://input'), true);
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if (isset($data['login'])) {
+
+    $username = $data['username'];
+    $password = $data['password'];
 
     $login = login($username, $password);
 
--- requests/search.php
+++ requests/search.php
@@ -5,3 +5,15 @@
  * Date: 6/14/2018
  * Time: 11:18 AM
  */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+if (isset($_POST['login'])) {
+
+    echo json_encode($_POST['login']);
+} else {
+	echo json_encode("no body");
+}
+
+?>
